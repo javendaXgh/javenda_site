ec69c724e457669dc07f651acf44c38841c02721
javendaXgh <javenda@gmail.com>
2021-02-07 23:30:29 -0400

    ni

---
+++ sync.R
@@ -0,0 +1,20 @@
+x <- rnorm(1:10)
+save(x, file = paste0("/Users/josemiguelavendanoinfante/R/sites/javenda_site/data-raw/arc.Rda"))
+
+
+
+
+
+system('git config user.email "javenda@gmail.com"')
+system('git config user.name "javendaXgh"')
+
+#system('git remote add origin /Users/josemiguelavendanoinfante/R/sites/javenda_site/.git')
+#system('git remote add p.jpeg https://github.com/javendaXgh/javenda_site')
+#system('git remote add .')
+system('git add /Users/josemiguelavendanoinfante/R/sites/javenda_site/data-raw/arc.Rda')
+system('git commit -am "x_prueba"')
+#system('git status')
+
+#system('git branch -a')
+system('git push https://github.com/javendaXgh/javenda_site master')#main master remotes/origin/HEAD
+#system(' git branch -a')
