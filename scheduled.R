2a7c7eb0df7297021cf3af40a51d21750fe9f84c
javendaXgh <javenda@gmail.com>
2021-02-25 01:36:54 -0400

    Commit covid 2021-02-25 01:36:54

--- scheduled.R
+++ scheduled.R
@@ -11,9 +11,9 @@ library(DT)
 library(dplyr)
 library(tidyverse)
 
-Sys.setenv(RSTUDIO_PANDOC="Applications/RStudio.app/Contents/MacOS/pandoc")
-setwd('/Users/josemiguelavendanoinfante/R/javenda_site')
-source('/Users/josemiguelavendanoinfante/R/shiny/app_covid_ven/actualizacion.R')
+Sys.setenv(RSTUDIO_PANDOC="/usr/lib/rstudio/bin/pandoc")
+setwd('/home/jose/R/javenda_site')
+source('/home/jose/R/shiny/app_covid_ven/actualizacion.R')
 #file.copy('/Users/josemiguelavendanoinfante/R/shiny/app_covid_ven/covid_intento.html','/Users/josemiguelavendanoinfante/R/javenda_site/static/covid19venezuela.html', 
 #overwrite = TRUE)
 #0 */3 * * *  ######3hours cron setting
@@ -22,28 +22,16 @@ source('/Users/josemiguelavendanoinfante/R/shiny/app_covid_ven/actualizacion.R')
 #https://docs.ropensci.org/git2r/index.html
 #https://github.com/ropensci/git2r/issues/296
 
-repo <- git2r::repository('/Users/josemiguelavendanoinfante/R/javenda_site')
-git2r::config(repo, user.name = 'javendaXgh',user.email = 'javendaXgh@gmail.com')
+repo <- git2r::repository('/home/jose/R/javenda_site')
+#git2r::config(repo, user.name = 'javendaXgh',user.email = 'javendaXgh@gmail.com')
 #git2r::cred_ssh_key()
 cred <- git2r::cred_ssh_key(
-  publickey = "/Users/josemiguelavendanoinfante/.ssh/id_rsa.pub",
-  privatekey = "/Users/josemiguelavendanoinfante/.ssh/id_rsa")
+  publickey = "/home/jose/.ssh/id_rsa.pub",
+  privatekey = "/home/jose/.ssh/id_rsa")
 
 git2r::cred_token()
-#push(repo, credentials = cred_token())#probar
-
-
-
-#x <- rnorm(1:10)
-#nombre <-paste0("static/covid19venezuela.html")
-
-#saveRDS (x, paste0('/Users/josemiguelavendanoinfante/R/pruebas_github/',nombre))
-#x <- rnorm(1:10)
-#save(x, file = paste0("/Users/josemiguelavendanoinfante/R/pruebas_github/data/data_", make.names(Sys.time()), ".Rda"))
-#setwd('/Users/josemiguelavendanoinfante/R/pruebas_github/data')
 git2r::add(repo,'*')
 git2r::commit(repo,paste('Commit covid', Sys.time()))
-#git2r::libgit2_features()
 git2r::config(repo, user.name = 'javendaXgh',user.email = 'javendaXgh@gmail.com')
 
 git2r::push(credentials = cred)
