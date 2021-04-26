0855a4e57bf7a076e6fd7ec3e9b980736ea7ea15
javendaXgh <javendaXgh@gmail.com>
2021-04-26 03:52:41 -0400

    cambios ortografia

--- script varios.R
+++ script varios.R
@@ -8,16 +8,7 @@ df_hospitales <- readRDS('bdmap/df_hospitales_seleccion_mapa.rds')
 write.xlsx(df_hospitales,'static/data/centros_medicos.xls', sheetName = 'cen_medicos', col.names = TRUE, append = FALSE)
 
 
-.Rproj.user
-.Rhistory
-.RData
-.Ruserdata
-javenda_site.Rproj
-.gitignore
-assets/
-  config
-config.toml
-config.yaml
+
 content/
   data-raw/
   laoyuts/
